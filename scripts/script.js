9024aa83288b3f7ff00d3cab7628d32ec00408da
Alex Gorodov <a.gorodov.22@gmail.com>
2022-07-03 07:54:11 +0300

    a lot of changes

--- scripts/script.js
+++ scripts/script.js
@@ -3,3 +3,21 @@ let upButton = document.querySelector(".footer-to-top");
 upButton.onclick = function() {
   window.scrollTo(0, 0);
 };
+
+let tutors = Array.from(document.querySelectorAll(".tutors-card"));
+
+let tutorsPrev = document.querySelector(".swiper-button-prev");
+let tutorsNext = document.querySelector(".swiper-button-next");
+
+console.log("tutors amount: " + tutors.length);
+
+tutorsNext.onclick = function () {
+  console.log("next pressed");
+  for (let i = 0; i <= tutors.length - 1; i++) {
+    if (i !== tutors.length - 1) {
+      let temp = tutors[i];
+      tutors[i+1] = tutors[i];
+      tutors[tutors.length - 1] = temp;
+    }
+  }
+};
