546203d2efac17bd2b48b9948446e96f9e33802a
Alex Gorodov <a.gorodov.22@gmail.com>
2022-07-04 18:42:19 +0300

    media

--- scripts/script.js
+++ scripts/script.js
@@ -4,20 +4,26 @@ upButton.onclick = function() {
   window.scrollTo(0, 0);
 };
 
+
+
 let tutors = Array.from(document.querySelectorAll(".tutors-card"));
 
 let tutorsPrev = document.querySelector(".swiper-button-prev");
 let tutorsNext = document.querySelector(".swiper-button-next");
 
-console.log("tutors amount: " + tutors.length);
+let ww = window.innerWidth;
 
-tutorsNext.onclick = function () {
-  console.log("next pressed");
-  for (let i = 0; i <= tutors.length - 1; i++) {
-    if (i !== tutors.length - 1) {
-      let temp = tutors[i];
-      tutors[i+1] = tutors[i];
-      tutors[tutors.length - 1] = temp;
+let containers = Array.from(document.querySelectorAll(".section-container"));
+
+console.log(containers.length);
+
+let containerWidth = function () {
+  for (let i = 0; i < containers.length; i++) {
+    if (ww < 800) {
+      console.log('Less than 800');
+      containers[i].classList.add("one-columned-container");
     }
   }
 };
+
+containerWidth();
