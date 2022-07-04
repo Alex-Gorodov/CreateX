b5828ecaca8813c238a3df302b955695d38c7229
Alex Gorodov <a.gorodov.22@gmail.com>
2022-07-04 21:23:25 +0300

    certificate positioning

--- scripts/script.js
+++ scripts/script.js
@@ -10,20 +10,3 @@ let tutors = Array.from(document.querySelectorAll(".tutors-card"));
 
 let tutorsPrev = document.querySelector(".swiper-button-prev");
 let tutorsNext = document.querySelector(".swiper-button-next");
-
-let ww = window.innerWidth;
-
-let containers = Array.from(document.querySelectorAll(".section-container"));
-
-console.log(containers.length);
-
-let containerWidth = function () {
-  for (let i = 0; i < containers.length; i++) {
-    if (ww < 800) {
-      console.log('Less than 800');
-      containers[i].classList.add("one-columned-container");
-    }
-  }
-};
-
-containerWidth();
