474161a7480e32c2e724cfced0415bf0b7d1b07e
Alex Gorodov <a.gorodov.22@gmail.com>
2022-07-07 01:33:41 +0300

    burger
    TODO layout of:
    - header section
    - certificate section

--- scripts/script.js
+++ scripts/script.js
@@ -4,9 +4,24 @@ upButton.onclick = function() {
   window.scrollTo(0, 0);
 };
 
+let burger = document.querySelector(".nav-burger .burger-nav-list");
+
+let firstLine = document.querySelector(".burger-line:nth-of-type(1)");
+let secondLine = document.querySelector(".burger-line:nth-of-type(2)");
+let thirdLine = document.querySelector(".burger-line:nth-of-type(3)");
+
+function burgerOpen() {
+    burger.classList.toggle("nav-list-opened");
+
+    firstLine.classList.toggle("burger-open-first-line");
+    secondLine.classList.toggle("burger-open-second-line");
+    thirdLine.classList.toggle("burger-open-third-line");
+}
 
 
-let tutors = Array.from(document.querySelectorAll(".tutors-card"));
 
-let tutorsPrev = document.querySelector(".swiper-button-prev");
-let tutorsNext = document.querySelector(".swiper-button-next");
+// let tutors = Array.from(document.querySelectorAll(".tutors-card"));
+
+// let tutorsPrev = document.querySelector(".swiper-button-prev");
+// let tutorsNext = document.querySelector(".swiper-button-next");
+
