194b56e9e4659aae8dd429dbfc1dc4223874e13b
Alex Gorodov <a.gorodov.22@gmail.com>
2022-07-13 03:42:23 +0300

    creating courses elements

--- scripts/courses-page-script.js
+++ scripts/courses-page-script.js
@@ -2,10 +2,27 @@
 
 const container = document.querySelector(".filters-list");
 const courseButtons = document.querySelectorAll(".filter-item .button");
+const coursesList = Array.from(document.querySelectorAll(".course-card"));
+
+let courseType = document.querySelectorAll(".course-type-badge");
+let courseName = document.querySelectorAll(".course-name");
+let courseAuthor = document.querySelectorAll(".course-author");
+let coursePrice = document.querySelectorAll(".course-price");
 
 // Taken from StackOverFlow
 // Add a click listener to the container
 container.addEventListener('click', handleClick, false);
+
+for (let i = 0; i < coursesList.length; i++) {
+  coursesList[i] = {
+    type: courseType[i].textContent,
+    name: courseName[i].textContent,
+    author: courseAuthor[i].textContent,
+    price: coursePrice[i].textContent,
+  };
+}
+
+// console.log(coursesList[2].type);
 
 function handleClick(e) {
   const { target } = e;
@@ -20,3 +37,16 @@ function handleClick(e) {
     target.classList.remove('inactive-btn');
   }
 }
+
+// return to console name of a course that badge mathes with clicked button
+for (let n = 0; n < coursesList.length; n++) {
+  courseButtons[n].onclick = function () {
+      for (let j = 0; j < coursesList.length; j++) {
+        if ((courseButtons[n].textContent.substring(0, 5)).match(coursesList[j].type.substring(0, 5))) {
+          console.log(coursesList[j].name);
+        }
+      
+    }
+  };
+}
+
