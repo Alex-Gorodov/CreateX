b5016f00d04d308db0845f667f0b4ed93ff13a28
Alex Gorodov <a.gorodov.22@gmail.com>
2022-08-03 22:16:13 +0300

    course script

--- scripts/courses-page-script.js
+++ scripts/courses-page-script.js
@@ -1,7 +1,7 @@
 // Course list filters toggler
-const container = document.querySelector(".filters-list");
+const coursesFilter = document.querySelector(".filters-list");
 const courseButtons = document.querySelectorAll(".filter-item .button");
-const coursesList = Array.from(document.querySelectorAll(".course-card"));
+let coursesList = Array.from(document.querySelectorAll(".course-card"));
 
 let courseType = document.querySelectorAll(".course-type-badge");
 let courseName = document.querySelectorAll(".course-name");
@@ -10,17 +10,7 @@ let coursePrice = document.querySelectorAll(".course-price");
 
 // Taken from StackOverFlow
 // Add a click listener to the container
-container.addEventListener('click', handleClick, false);
-
-for (let i = 0; i < coursesList.length; i++) {
-  coursesList[i] = {
-    type: courseType[i].textContent,
-    name: courseName[i].textContent,
-    author: courseAuthor[i].textContent,
-    price: coursePrice[i].textContent,
-  };
-}
-
+coursesFilter.addEventListener('click', handleClick, false);
 function handleClick(e) {
   const { target } = e;
 
@@ -35,18 +25,29 @@ function handleClick(e) {
   }
 }
 
-function compare( a, b ) {
-  if ( a.type < b.type ){
-    return -1;
-  }
-  if ( a.type > b.type ){
-    return 1;
-  }
-  return 0;
+for (let i = 0; i < coursesList.length; i++) {
+  coursesList[i] = {
+    type: courseType[i].textContent,
+    name: courseName[i].textContent,
+    author: courseAuthor[i].textContent,
+    price: coursePrice[i].textContent,
+  };
 }
 
-coursesList.sort( compare );
+for (const element of courseButtons) {
+  courseButtons.forEach(element => {
+    element.onclick = function () {
+      let result = coursesList.filter(course =>
+        element.textContent.match(course.type));
+        console.log(result);
+        // console.log(coursesList.length);
+        // coursesList = result;
+        // console.log(coursesList.length);
+    };
+  });
+}
 
+// sections animation
 function onEntry(entry) {
   entry.forEach(change => {
     if (change.isIntersecting) {
